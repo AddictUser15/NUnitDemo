872624009c12c02a4be1ddf4abd6a70a19f6c71b
Monika <monika@ericsson.com>
2017-11-14 19:56:48 +0530

    Add existing file

---
+++ ConsoleApp.Test/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ConsoleApp;
+
+namespace ConsoleApp.Test
+{
+    [TestFixture]
+    public class Class1
+    {
+        [SetUp]
+        public void SetupMethod()
+        {
+            System.Diagnostics.Debug.WriteLine("Starting test execution");
+        }
+        [TestCase]
+        [Category("AdditionTests")]
+        public void TestAdd()
+        {
+            Add objadd = new Add();
+            int addition = objadd.AddValues(25, 35);
+            Assert.AreEqual(60, addition);
+        }
+        [TestCase]
+        [Category("AdditionTests")]
+        public void TestAddFail()
+        {
+            Add objadd = new Add();
+            int addition = objadd.AddValues(25, 35);
+            Assert.AreEqual(6, addition);
+        }
+        [TestCase]
+        [Category("GreaterTests")]
+        public void TestAddGreater()
+        {
+            Add objadd = new Add();
+            int addition = objadd.AddValues(25, 35);
+            Assert.Greater(100, addition);
+        }
+        [TestCase]
+        [Ignore("Skipping TestAddIgnore for Demo purpose")]
+        public void TestAddIgnore()
+        {
+            Add objadd = new Add();
+            int addition = objadd.AddValues(25, 35);
+            Assert.Greater(100, addition);
+        }
+        [TestCase]
+        public void TestAddException()
+        {
+            Assert.Throws<ArgumentException>(() => { throw new ArgumentException(); });
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Add
+    {
+        public Add()
+        { }
+        public int AddValues(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
